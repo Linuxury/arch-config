c17e43236f04e03e664f2a2b558cd26c237d33ce
Linuxury <161166960+Linuxury@users.noreply.github.com>
2026-02-17 21:12:30 -0500

    Delete config.lua

--- config.lua
+++
@@ -1,8 +0,0 @@
--- dcli configuration pointer
--- This file points to the active host configuration
--- The full configuration lives in hosts/Ryzen5900x.lua
-
--- Active host
-return {
-    host = "Ryzen5900x", "ThinkPad",
-}
